9c9efb8ac6f4100b42bdb648079221bb3d42e32f
pnytko <pnytko98@gmail.com>
2022-11-01 10:54:26 +0100

    popup draggable

--- script.js
+++ script.js
@@ -302,3 +302,24 @@ var markers = new ol.layer.Vector({
   var marker2 = new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat([20.9972, 49.9891])));
   markers.getSource().addFeature(marker1);
   markers.getSource().addFeature(marker2);
+
+  const wrapper = document.querySelector(".wrapper"),
+  header = wrapper.querySelector("header")
+
+  function onDrag({movementX, movementY}){
+    let getStyle = window.getComputedStyle(wrapper);
+    let left = parseInt (getStyle.left);
+    let top = parseInt (getStyle.top);
+    wrapper.style.left = `${left + movementX}px`
+    wrapper.style.top = `${top + movementY}px`
+}
+
+
+  header.addEventListener("mousedown", ()=>{
+    header.addEventListener("mousemove", onDrag);
+  });
+
+  document.addEventListener("mouseup", ()=>{
+    header.removeEventListener("mousemove", onDrag);
+
+  });
